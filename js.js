5294cdc0bf2b7cbc79ab155829fa1e5680d33772
Stepanich228 <129016101+Stepanich228@users.noreply.github.com>
2023-12-07 19:31:10 +1000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,54 @@
+// const hello = 'privet mir. poka mir';
+// const key = 'mir';
+// const firstPos = hello.indexOf(key , 10);
+// console.log('Pervoe poyavlenie: ', firstPos);
+
+
+
+// const hello = 'privet mir. poka mir';
+
+// console.log(hello.includes('mir'));
+// console.log(hello.includes('mig'));
+
+// const alex ={
+//     name: 'Саньку',
+//     age:19
+
+// }
+// const alexInfo = `${alex.name} сейчас ${alex.age} лет`;
+// console.log(alexInfo);
+
+
+
+// function sum(x,y){
+//     return x + y;
+
+// }
+// const a = 5;
+// const b = 6;
+
+// const resalt = `${a} + ${b} = ${sum(a,b)}`;
+// console.log(resalt);
+
+// const expression = `${a} * ${b} = ${a*b}`;
+// console.log(expression);
+
+// const alex = {name: 'Misha', age: 17};
+// const vitya = {name: 'Sasha', age: 31};
+
+// function check (parts, name, age){
+//     if(age > 17) return `${parts[0]}${name}. доступ открыт `;
+//     else return `для пльзователя ${name} доступ закрыт. Возраст ${age} недействителен.`;
+
+// }
+// let checkedTemplade1 = check`Пользователь: ${alex.name} ${alex.age}`;
+// let checkedTemplade2 = check`Пользователь: ${vitya.name} ${vitya.age}`;
+
+// console.log(checkedTemplade1);
+// console.log(checkedTemplade2);
+
+
+// const str2 = 'Hellow World';
+// const exp2 = /world/i;
+// console.log(exp2.test(str2));
+
